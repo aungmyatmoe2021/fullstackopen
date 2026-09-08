9eb4c913cebc627c1fc14aa29df4ee55b02267b6
aungmyatmoe2021 <aungmyatmoe2021@gmail.com>
2026-09-08 14:31:00 +0100

    1.3: Course Information step 3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props);
   return <h1>{props.course}</h1>;
 };
 
@@ -40,11 +41,23 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   const course_details = [
-    { part: "Fundamentals of React", exercises: 10 },
-    { part: "Using props to pass data", exercises: 7 },
-    { part: "State of a component", exercises: 14 },
+    { part: part1.name, exercises: part1.exercises },
+    { part: part2.name, exercises: part2.exercises },
+    { part: part3.name, exercises: part3.exercises },
   ];
   return (
     <>
